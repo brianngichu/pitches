2fe3fc3f1ec2d4473697e521a5985c8b3be344b2
Ngichu <brianngichu@gmail.com>
2019-07-02 13:27:19 +0300

    deployment

--- config.py
+++ config.py
@@ -15,8 +15,8 @@ class Config:
     #SIMPLEMDE_USE_CDN = True
     
 class ProdConfig(Config):
-    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #pass
 class DevConfig(Config):
     DEBUG = True
 
