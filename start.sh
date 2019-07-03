0670521e910e89346d11d46d23b3a7a60acefbb1
Ngichu <brianngichu@gmail.com>
2019-07-03 15:11:46 +0300

    export database url

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MAIL_USERNAME='briangithire@gmail.com'
 export MAIL_PASSWORD='Christians727'
 export SECRET_KEY='brianngichu123456789'
-
+SQLALCHEMY_DATABASE_URI='DATABASE_URL'
 export DATABASE_URL='postgresql+psycopg2://moringa:0000@localhost/pitch'
 python3.6 manage.py server
