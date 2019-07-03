0e4f04eec8eda0be01757d413035df1aa7ece4ff
Ngichu <brianngichu@gmail.com>
2019-07-03 15:16:39 +0300

    deployment

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ class Config:
     
     
 class ProdConfig(Config):
-    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    #pass
 class DevConfig(Config):
     DEBUG = True
 
